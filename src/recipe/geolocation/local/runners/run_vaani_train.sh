7bf9b20804433c1bf7a99384fb8c882596a860b8
Shikhar Bharadwaj <shikhar.ssu@gmail.com>
2025-11-04 23:36:10 -0600

    minor

--- src/recipe/geolocation/local/runners/run_vaani_train.sh
+++ src/recipe/geolocation/local/runners/run_vaani_train.sh
@@ -7,9 +7,10 @@
 #SBATCH -c 72
 #SBATCH --mem=120G
 #SBATCH -t 48:00:00
-#SBATCH -o /work/nvme/bbjs/sbharadwaj/powsm/PhoneBench/logs/vaani_geolocation/%x_%j.out
-#SBATCH -e /work/nvme/bbjs/sbharadwaj/powsm/PhoneBench/logs/vaani_geolocation/%x_%j.out
+#SBATCH -o /work/nvme/bbjs/sbharadwaj/powsm/PhoneBench/exp/vaani_geolocation/%x_%j.out
+#SBATCH -e /work/nvme/bbjs/sbharadwaj/powsm/PhoneBench/exp/vaani_geolocation/%x_%j.out
 
+mkdir -p /work/nvme/bbjs/sbharadwaj/powsm/PhoneBench/exp/vaani_geolocation
 # === Environment setup ===
 source ~/.bashrc
 conda activate powsmesp
