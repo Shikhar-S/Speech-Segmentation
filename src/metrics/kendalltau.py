f725b3a889677dfc51ba5552be0ec5002b1447de
Shikhar Bharadwaj <sbharad2@andrew.cmu.edu>
2025-12-21 19:06:51 -0500

    fix kendalltau gpu-cpu issue

--- src/metrics/kendalltau.py
+++ src/metrics/kendalltau.py
@@ -15,8 +15,8 @@ class KendallTau(Metric):
         self.targets.append(targets.detach().cpu().flatten())
 
     def compute(self):
-        preds_all = torch.cat(self.preds).numpy()
-        targets_all = torch.cat(self.targets).numpy()
+        preds_all = torch.cat(self.preds).cpu().numpy()
+        targets_all = torch.cat(self.targets).cpu().numpy()
         if len(np.unique(preds_all)) < 2 or len(np.unique(targets_all)) < 2:
             return torch.tensor(0.0, device=self.device)
         tau, _ = kendalltau(preds_all, targets_all)
