053e9e441fb97cf201ff5c5a1d718b93f4096715
Shikhar <shikhar.ssu@gmail.com>
2026-03-17 00:32:19 -0500

    rm ded

--- scripts/pr_timit_joint_train.sh
+++
@@ -1,9 +0,0 @@
-for mixing in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
-   echo "Submitting joint CTC+Attention training job with Epitran mix ratio: $mixing"
-   sbatch --time=2:30:00 -p ghx4 \
-        -c 72 --ntasks-per-node=1 --gpus-per-node=1 --mem=120G \
-        scripts/daixpr.batch \
-        experiment=train/timit_xeuspr_joint \
-        run_folder=xeus_timit.joint_ctc_att.mix${mixing}.5ksteps \
-        data.epitran_mix_ratio=$mixing
-done
